6423906eff34423cc9a5e20546eb52e17867bf09
oluwaponire <oluwaponire@Linux.myguest.virtualbox.org>
2023-08-24 23:12:40 +0100

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import {useState, React, useEffect} from 'react';
 
 
 const App = () => {
-  
+  const apikey = '7e2971bd';
+  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=7e2971bd';
+  const searchMovies = async (title) => {
+        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=7e2971bd&s=$title');
+        const data = await response.json();
+        console.log(data.Search);
+  };
+  useEffect(() => {
+      searchMovies('spiderman');
+  },[]);
   return (
     <div className="App">
     </div>
