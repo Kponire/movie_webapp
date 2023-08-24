4e4204d171c70b0c7fe885b72c13198966f1ca0d
oluwaponire <oluwaponire@Linux.myguest.virtualbox.org>
2023-08-24 23:40:15 +0100

    Added a moviecard component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,47 @@ import {useState, React, useEffect} from 'react';
 
 const App = () => {
   const apikey = '7e2971bd';
-  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=7e2971bd';
+  const apiurl = `http://www.omdbapi.com/?i=tt3896198&apikey=${apikey}`;
   const searchMovies = async (title) => {
-        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=7e2971bd&s=$title');
+        const response = await fetch(`${apiurl}&s=${title}`);
         const data = await response.json();
         console.log(data.Search);
   };
+  const inputMovies = () => {
+
+  }
+  const movies = () => {
+    <div className='container'>
+    <div className='movie'> 
+        <div>{movie1.year}</div>
+        <div> <img /></div>
+        <div>Type</div>
+        <div>Title</div>
+    </div>
+  </div>
+}
+  const movie1 = {
+    name: 'The Super Hero',
+    year: '2020',
+
+  }
   useEffect(() => {
       searchMovies('spiderman');
   },[]);
   return (
     <div className="App">
+      <h1> MovieLand </h1>
+      <div className='search'>
+        <input 
+          type='search' 
+          placeholder='Search for movies' 
+          name='search'
+          onChange={inputMovies}
+        />
+        <button type='submit'>
+          Search
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MovieCard = () => {
+    return (
+        <div className='container'>
+        <div className='movie'> 
+            <div>{movie1.year}</div>
+            <div> <img /></div>
+            <div>Type</div>
+            <div>Title</div>
+        </div>
+      </div>
+    );
+};
+
+export default MovieCard;
